025954d6109dba5e068efbb47e633fa9124f5d1d
AlexandruSerbicean <2236924@student.uma.pt>
2025-03-16 19:46:03 +0000

    Final step | Add the Servers and minimal modification based on refresh of counter

--- InfraSim/Services/UserCounter.cs
+++ InfraSim/Services/UserCounter.cs
@@ -9,16 +9,22 @@ namespace InfraSim.Services
         public int Counter { get; private set; } = 1;
         public event Action? OnCounterChanged;
 
+        public void ResetCounter()
+        {
+            Counter = 1; 
+            OnCounterChanged?.Invoke();
+        }
+
         public async Task StartIncrementing()
         {
             while (Counter < 200000)
             {
-                Counter += 1000; 
+                Counter += 1000;
                 OnCounterChanged?.Invoke();
-                await Task.Delay(10); 
+                await Task.Delay(10);
             }
-            
-            Counter = 200000; 
+
+            Counter = 200000;
             OnCounterChanged?.Invoke();
         }
     }

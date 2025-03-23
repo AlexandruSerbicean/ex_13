61b354d0299097a288e031e7befcb6e9ce9beb33
Alexandru Serbicean <2236924@student.uma.pt>
2025-03-23 00:50:35 +0000

    Additional tests

--- InfraSim.Tests/ServerCapabilityFactoryTests.cs
+++ InfraSim.Tests/ServerCapabilityFactoryTests.cs
@@ -4,11 +4,12 @@ using Xunit;
 
 public class ServerCapabilityFactoryTests
 {
+
     [Theory]
     [InlineData(ServerType.Server, 1000, 2500)]
     [InlineData(ServerType.Cache, 100000, 3500)]
     [InlineData(ServerType.LoadBalancer, 10000000, 4000)]
-    [InlineData(ServerType.CDN, 1000000000000, 55000)] 
+    [InlineData(ServerType.CDN, 1000000000000, 55000)]
     public void Factory_ShouldReturnCorrectCapability(ServerType type, long expectedRequests, int expectedCost)
     {
         var factory = new ServerCapabilityFactory();
@@ -18,5 +19,27 @@ public class ServerCapabilityFactoryTests
         Assert.Equal(expectedRequests, capability.MaximumRequests);
         Assert.Equal(expectedCost, capability.Cost);
     }
+
+    [Fact]
+    public void Factory_ShouldThrowException_ForUnsupportedType()
+    {
+        var factory = new ServerCapabilityFactory();
+
+        ServerType invalidType = (ServerType)999;
+
+        var ex = Assert.Throws<ArgumentOutOfRangeException>(() => factory.Create(invalidType));
+        Assert.Contains("Server type", ex.Message);
+    }
+
+        [Fact]
+    public void TemporaryStorageDecorator_ShouldDecorateProperly()
+    {
+        var baseCap = new ServerCapability();
+        var decorator = new TemporaryStorageDecorator(baseCap);
+
+        Assert.Equal(100000, decorator.MaximumRequests); 
+        Assert.Equal(3500, decorator.Cost);              
+    }
+
 
 }

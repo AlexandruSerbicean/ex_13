fa74f887ce917cf0d00744561c6d12dd7ee2039f
Alexandru Serbicean <2236924@student.uma.pt>
2025-03-25 22:58:35 +0000

    Updated IServer and create Base Server

--- InfraSim/Pages/Models/IServer.cs
+++ InfraSim/Pages/Models/IServer.cs
@@ -1,8 +1,37 @@
+using InfraSim.Pages.Models.Capabilities;
+
 namespace InfraSim.Pages.Models
 {
     public interface IServer
     {
         ServerType ServerType { get; }
-        void HandleRequests(int requestsCount);
+        IServerCapability Capability { get; }
+        int RequestsCount { get; set; }
+        void HandleRequests(int requests);
+    }
+
+    public abstract class BaseServer : IServer
+    {
+        private int Requests = 0;
+
+        public ServerType ServerType { get; private set; }
+        public IServerCapability Capability { get; private set; }
+
+        public int RequestsCount
+        {
+            get => Requests;
+            set => Requests = value;
+        }
+
+        public virtual void HandleRequests(int requests)
+        {
+            RequestsCount = requests;
+        }
+
+        public BaseServer(ServerType type, IServerCapability capability)
+        {
+            ServerType = type;
+            Capability = capability;
+        }
     }
 }

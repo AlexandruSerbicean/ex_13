43305beae0cf1dd252614422694dc39d3ac0a35c
Alexandru Serbicean <2236924@student.uma.pt>
2025-04-06 13:40:21 +0100

    Added IRepository Interface and Repository Class

---
+++ InfraSim/Pages/Models/Database/Repository.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InfraSim.Pages.Models.Database
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : DbItem
+    {
+        private readonly InfraSimContext Context;
+
+        public Repository(InfraSimContext context)
+        {
+            Context = context;
+        }
+
+        public List<TEntity> GetAll()
+        {
+            return Context.Set<TEntity>().ToList();
+        }
+
+        public TEntity? Get(Guid id)
+        {
+            return Context.Set<TEntity>().FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Insert(TEntity item)
+        {
+            Context.Set<TEntity>().Add(item);
+        }
+
+        public void Update(TEntity item)
+        {
+            Context.Entry(item).State = EntityState.Modified;
+        }
+
+        public void Delete(TEntity item)
+        {
+            Context.Set<TEntity>().Remove(item);
+        }
+    }
+}
